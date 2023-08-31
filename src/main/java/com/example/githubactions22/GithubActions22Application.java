ebefa1f6b2cf4ef29ee252ebef8334d11ca97a59
Thea6037 <thea6037@stud.kea.dk>
2023-08-31 15:03:24 +0200

    First commit

---
+++ src/main/java/com/example/githubactions22/GithubActions22Application.java
@@ -0,0 +1,15 @@
+package com.example.githubactions22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActions22Application
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(GithubActions22Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubactions22/GithubActions22ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.githubactions22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActions22ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
